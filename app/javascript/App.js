3e4ac588b74ded3f4592376eeda4ea4ba5798e9f
GordonOtieno <otienogordon95@gmail.com>
2023-02-20 16:14:18 +0300

    Create greeting component and set up redux

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import Greeting from './Greeting'
+import store from './redux/store'
+import { Provider } from 'react-redux'
 
 const App = () => {
   return (
-    <div>
-      Hello world
-    </div>
+      <Provider store={store}>
+          <Greeting />
+      </Provider>
   )
 }
 
-export default App
+export default App;
